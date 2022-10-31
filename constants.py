b56264d09d1b728ddad054e3f170100d900f863b
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-10-31 17:34:58 -0400

    made a change

---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by Emmanuel Fofeyin
+# Created on: Oct 2022
+# This constants file is for space Alien game
+
+#PyBadge screen size in 160x128 and sprites are 16x16
+SCREEN_X = 60
+SCRERN_y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
